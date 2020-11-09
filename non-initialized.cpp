0c8466bd5a02013e4745585751dc922b7650e699
林秉毅 <linbingyi@huawei.com>
2020-11-09 13:34:21 +0800

    commit

--- non-initialized.cpp
+++ non-initialized.cpp
@@ -5,41 +5,9 @@
 
 using namespace std;
 
-void check_magic_number(void)
-{
-	short tmp = (short)(0x1234 << 8);
-	cout << tmp << endl;
 
-	char c = ((char *)&tmp)[0];
-	cout << c << endl;
-	int i = 0 << (int)c;
-
-	cout << i << endl;
-}
-
-void foo(const char* input)
-{
-	char buf[10];
-	printf("My stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
-	strcpy(buf, input);
-	printf("buf = %s\n", buf);
-	printf("Now the stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
-}
-void bar(void)
-{
-	printf("Augh! I've been hacked!\n");
-}
 int main(int argc, char* argv[])
 {
-//	check_magic_number();
-//	printf("Address of foo = %p\n", foo);
-//	printf("Address of bar = %p\n", bar);
-	//if (argc != 2)
-	//{
-	//	printf("Please supply a string as an argument!\n");
-	//	return -1;
-	//}
-//	foo(argv[1]);
 
 	for(int i = 0; i < 10; i ++){
 		int randomNum = rand() % 10;
