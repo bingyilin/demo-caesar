93290ed882b8427b0f0043735aebbb408dba50a2
林秉毅 <linbingyi@huawei.com>
2020-11-09 16:09:37 +0800

    commit

---
+++ BufferOverflow.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream> 
+
+using namespace std;
+
+
+void foo(const char* input)
+{
+	char buf[10];
+	printf("My stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
+	strcpy(buf, input);
+	printf("buf = %s\n", buf);
+	printf("Now the stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
+}
+void bar(void)
+{
+	printf("Augh! I've been hacked!\n");
+}
+int main(int argc, char* argv[])
+{
+	printf("Address of foo = %p\n", foo);
+	printf("Address of bar = %p\n", bar);
+	foo("ABCDEFGHIJKLMN");
+	printf("Exit!\n");
+	return 0;
+}
--- divideByZero.cpp
+++ divideByZero.cpp
@@ -6,20 +6,20 @@
 using namespace std;
 
 
-int main(int argc, char* argv[])
-{
-	char zeroC = 'NUL';
-	int a = zeroC >> 8;
-	cout << a << endl;
-	int result = 100 / a;
-	cout << result << endl;
-	//for (int i = 0; i < 10; i++) {
-	//	int randomNum = rand() % 10;
-	//	int result = 100 / randomNum;
-	//	cout << result << endl;
-	//}
-
-
-	printf("Exit!\n");
-	return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//	char zeroC = 'NUL';
+//	int a = zeroC >> 8;
+//	cout << a << endl;
+//	int result = 100 / a;
+//	cout << result << endl;
+//	//for (int i = 0; i < 10; i++) {
+//	//	int randomNum = rand() % 10;
+//	//	int result = 100 / randomNum;
+//	//	cout << result << endl;
+//	//}
+//
+//
+//	printf("Exit!\n");
+//	return 0;
+//}
