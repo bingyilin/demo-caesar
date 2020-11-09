8da4447facdf701f263918e5c1b63d0ea0b42f3a
林秉毅 <linbingyi@huawei.com>
2020-11-09 13:42:08 +0800

    commit

--- non-initialized.cpp
+++ non-initialized.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include <time.h>  
 
 using namespace std;
 
@@ -9,8 +10,9 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
+	srand((unsigned)time(NULL));
 	for(int i = 0; i < 10; i ++){
-		int randomNum = 5*5-25;
+		int randomNum = rand() % 10;
 		int result = 100 /randomNum;
 		cout << result << endl;
 	}
