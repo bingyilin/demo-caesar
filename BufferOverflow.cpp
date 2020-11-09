e7c7084621681e6c578c04a905efa3b177cc7e6f
林秉毅 <linbingyi@huawei.com>
2020-11-09 16:11:26 +0800

    commit

--- BufferOverflow.cpp
+++ BufferOverflow.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void foo(const char* input)
 {
 	char buf[10];
-	printf("My stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
+	//printf("My stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
 	strcpy(buf, input);
 	printf("buf = %s\n", buf);
 	printf("Now the stack looks like:\n%p\n%p\n%p\n%p\n%p\n%p\n%p\n\n");
@@ -20,8 +20,8 @@ void bar(void)
 }
 int main(int argc, char* argv[])
 {
-	printf("Address of foo = %p\n", foo);
-	printf("Address of bar = %p\n", bar);
+	//printf("Address of foo = %p\n", foo);
+	//printf("Address of bar = %p\n", bar);
 	foo("ABCDEFGHIJKLMN");
 	printf("Exit!\n");
 	return 0;
