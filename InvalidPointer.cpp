4c8a1ea204b8f20a115d5c37a1a409749e464a3a
林秉毅 <linbingyi@huawei.com>
2020-11-09 17:08:17 +0800

    commit

---
+++ InvalidPointer.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream> 
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	char *p = (char *)malloc(100);
+
+	strcpy(p, "hello");
+	cout << p << endl;
+	free(p); // p 所指的内存被释放，但是p所指的地址仍然不变
+
+	cout << p << endl;
+	return 0;
+}
