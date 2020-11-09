613e26cdb7e8faf71512a83ec77dc912c08b7650
林秉毅 <linbingyi@huawei.com>
2020-11-09 15:11:30 +0800

    commit

--- non-initialized.cpp
+++ non-initialized.cpp
@@ -2,22 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
-#include <time.h>  
+//#include <time.h>  
 
 using namespace std;
 
 
-int main(int argc, char* argv[])
-{
-
-	srand((unsigned)time(NULL));
-	for(int i = 0; i < 10; i ++){
-		int randomNum = rand() % 10;
-		int result = 100 /randomNum;
-		cout << result << endl;
-	}
-
-
-	printf("Exit!\n");
-	return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//
+////	srand((unsigned)time(NULL));
+//	for(int i = 0; i < 10; i ++){
+//		int randomNum = rand() % 10;
+//		int result = 100 /randomNum;
+//		cout << result << endl;
+//	}
+//
+//
+//	printf("Exit!\n");
+//	return 0;
+//}
