d8ff8bf556f830ffaed7fc57b996f08c6274e07e
林秉毅 <linbingyi@huawei.com>
2020-11-09 15:47:28 +0800

    commit

--- divideByZero.cpp
+++ divideByZero.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-
-	for (int i = 0; i < 10; i++) {
-		int randomNum = rand() % 10;
-		int result = 100 / randomNum;
-		cout << result << endl;
-	}
+	int a = atoi(argv[1]);
+	int result = 100 / a;
+	cout << result << endl;
+	//for (int i = 0; i < 10; i++) {
+	//	int randomNum = rand() % 10;
+	//	int result = 100 / randomNum;
+	//	cout << result << endl;
+	//}
 
 
 	printf("Exit!\n");
