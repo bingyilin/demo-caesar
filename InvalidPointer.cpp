390e4c3cba9416c862b79df8afcb9ae5a3ad1ee7
林秉毅 <linbingyi@huawei.com>
2020-11-09 19:19:07 +0800

    commit

--- InvalidPointer.cpp
+++ InvalidPointer.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 
-int main(int argc, char* argv[])
-{
-	char *p = 0;
-
-	strcpy(p, "hello");
-	cout << p << endl;
-	free(p); // p 所指的内存被释放，但是p所指的地址仍然不变
-
-	cout << p << endl;
-	return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//	char *p = 0;
+//
+//	strcpy(p, "hello");
+//	cout << p << endl;
+//	free(p); // p 所指的内存被释放，但是p所指的地址仍然不变
+//
+//	cout << p << endl;
+//	return 0;
+//}
