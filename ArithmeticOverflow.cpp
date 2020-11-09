6f882bf06f10b337c5d89647abfa0ba4d95bad28
林秉毅 <linbingyi@huawei.com>
2020-11-09 19:29:47 +0800

    提交

--- ArithmeticOverflow.cpp
+++ ArithmeticOverflow.cpp
@@ -1,15 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <iostream> 
-
-using namespace std;
-
-
-int main(int argc, char* argv[])
-{
-	int i = 2147483647;
-	i++;
-	cout << i << endl;
-	return 0;
-}
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <iostream> 
+//
+//using namespace std;
+//
+//
+//int main(int argc, char* argv[])
+//{
+//	int i = 2147483647;
+//	i++;
+//	cout << i << endl;
+//	return 0;
+//}
---
+++ NanFloat.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream> 
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	float i = sqrt(-1.0);
+	i = i * 1.0;
+	cout << i << endl;
+}
