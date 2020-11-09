44fa7d7d13a02a3db8f79dbb643ed6966bf15a7b
林秉毅 <linbingyi@huawei.com>
2020-11-09 15:13:08 +0800

    commit

---
+++ divideByZero.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream> 
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+
+	for (int i = 0; i < 10; i++) {
+		int randomNum = 0;
+		int result = 100 / randomNum;
+		cout << result << endl;
+	}
+
+
+	printf("Exit!\n");
+	return 0;
+}
