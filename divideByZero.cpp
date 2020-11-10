b78d9d8ae2a5b96070a1454bac427af127dfadb0
林秉毅 <linbingyi@huawei.com>
2020-11-10 16:26:28 +0800

    commit

--- divideByZero.cpp
+++ divideByZero.cpp
@@ -34,19 +34,19 @@ int char2int(const char* str) {
 int main(int argc, char* argv[])
 {
 
-	char* c = argv[1];
-	int a = char2int(c);
+	//char* c = argv[1];
+	//int a = char2int(c);
 
 	//char zeroC = 'NUL';
 	//int a = zeroC >> 8;
 	//cout << a << endl;
-	int result = 100 / a;
-	cout << result << endl;
-	//for (int i = 0; i < 10; i++) {
-	//	int randomNum = rand() % 10;
-	//	int result = 100 / randomNum;
-	//	cout << result << endl;
-	//}
+	//int result = 100 / a;
+	//cout << result << endl;
+	for (int i = 0; i < 10; i++) {
+		int randomNum = rand() % 10;
+		int result = 100 / randomNum;
+		cout << result << endl;
+	}
 
 
 	printf("Exit!\n");
