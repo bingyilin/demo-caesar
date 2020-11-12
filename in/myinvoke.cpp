d3ac19b4f4ee02021eefc9e44cfd61d296fd0bba
林秉毅 <linbingyi@huawei.com>
2020-11-12 13:50:26 +0800

    commit

--- in/myinvoke.cpp
+++ in/myinvoke.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void give_test_for_divide() {
 	int i;
 	i = 0;
-//	scanf("%d", &i);  //&i 表示变量 i 的地址，&是取地址符
+	scanf("%d", &i);  //&i 表示变量 i 的地址，&是取地址符
 	printf("i = %d\n", i);
 	int num = INT_MAX;
 	num = num / i;
