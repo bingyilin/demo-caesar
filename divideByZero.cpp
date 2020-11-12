b1db3c9f70789f73cb4f57bb068b3cfa66ccc3cb
林秉毅 <linbingyi@huawei.com>
2020-11-12 09:30:54 +0800

    commit

--- divideByZero.cpp
+++ divideByZero.cpp
@@ -47,27 +47,27 @@ int readTxt()
 }
 
 
-//int main(int argc, char* argv[])
-//{
-//
-//	readTxt();
-//	//char* c = argv[1];
-//	//int a = char2int(c);
-//
-//	//char zeroC = 'NUL';
-//	//int a = zeroC >> 8;
-//	//cout << a << endl;
-//
-//	//int result = 100 / a;
-//	//cout << result << endl;
-//	//result = result / 0;
-//	//for (int i = 0; i < 10; i++) {
-//	//	int randomNum = rand() % 10;
-//	//	int result = 100 / randomNum;
-//	//	cout << result << endl;
-//	//}
-//
-//
-//	printf("Exit!\n");
-//	return 0;
-//}
+int nomain(int argc, char* argv[])
+{
+
+	readTxt();
+	//char* c = argv[1];
+	//int a = char2int(c);
+
+	//char zeroC = 'NUL';
+	//int a = zeroC >> 8;
+	//cout << a << endl;
+
+	//int result = 100 / a;
+	//cout << result << endl;
+	//result = result / 0;
+	//for (int i = 0; i < 10; i++) {
+	//	int randomNum = rand() % 10;
+	//	int result = 100 / randomNum;
+	//	cout << result << endl;
+	//}
+
+
+	printf("Exit!\n");
+	return 0;
+}
